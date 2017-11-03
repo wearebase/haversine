0788c58b001e568406daa1dc412792f5418a2f4f
Manuel Martin <manuel.martin@wearebase.com>
2017-11-03 14:46:40 +0000

    Calculate bearing in the same function

--- haversine/__init__.py
+++ haversine/__init__.py
@@ -1,9 +1,9 @@
-from math import radians, cos, sin, asin, sqrt
+from math import radians, cos, sin, asin, sqrt, atan2
 
 AVG_EARTH_RADIUS = 6371  # in km
 
 
-def haversine(point1, point2, miles=False):
+def haversine(point1, point2, miles=False, bearing=False):
     """ Calculate the great-circle distance between two points on the Earth surface.
 
     :input: two 2-tuples, containing the latitude and longitude of each point
@@ -11,9 +11,10 @@ def haversine(point1, point2, miles=False):
 
     Example: haversine((45.7597, 4.8422), (48.8567, 2.3508))
 
-    :output: Returns the distance bewteen the two points.
+    :output: Returns the distance between the two points.
     The default unit is kilometers. Miles can be returned
-    if the ``miles`` parameter is set to True.
+    if the ``miles`` parameter is set to True. Bearing can additionally
+    be returned if the ``bearing`` parameter is set to True.
 
     """
     # unpack latitude/longitude
@@ -27,9 +28,15 @@ def haversine(point1, point2, miles=False):
     lat = lat2 - lat1
     lng = lng2 - lng1
     d = sin(lat * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(lng * 0.5) ** 2
-    h = 2 * AVG_EARTH_RADIUS * asin(sqrt(d))
-    if miles:
-        return h * 0.621371  # in miles
-    else:
-        return h  # in kilometers
+    h = 2 * AVG_EARTH_RADIUS * asin(sqrt(d))  # in kilometers
 
+    if miles:
+        h = h * 0.621371  # in miles
+
+    if bearing:
+        y = sin(lng) * cos(lat2);
+        x = cos(lat1) * sin(lat2) - sin(lat1) * cos(lat2) * cos(lng);
+        bearing = atan2(y, x)  # in radians
+        return h, bearing
+
+    return h
